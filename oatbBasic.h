656307d95cbfb38effc1a42fa84d80b6f30a5dc2
Bananyse <watcrystal@163.com>
2019-12-17 04:15:13 +0800

    init

---
+++ oatbBasic.h
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define forr(_I,_N) for(int _I=0;_I<(_N);_I++)
+
+const int N = 16;
+const int M = 6;
+
+typedef std::pair<int,int> ABpair;
+typedef std::array<int,M> arr;
+typedef std::vector<arr> pool;
+
+char toChar(int x)
+{
+	if(x<0) return '-';
+	if(x<10) return '0'+x;
+	if(x<36) return 'A'+x-10;
+	return '+';
+}
+	
+ABpair oatb(arr X,arr Y)
+{
+	int A=0,B=0;
+	int flag[N]={0};
+	forr(i,M) flag[X[i]]=i+1;
+	forr(i,M)
+	{
+		int tmp=flag[Y[i]];
+		if(tmp==i+1) A++;
+		else if(tmp) B++;
+	}
+	return std::make_pair(A,B);
+}
--- oatbSolver.cpp
+++ oatbSolver.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
+#include "oatbBasic.h"
 
 using namespace std;
+
+typedef std::pair<int,int> ABpair;
+typedef std::array<int,M> arr;
+typedef std::vector<arr> pool;
+
+class Condition
+{
+	
+};
 
 int main()
 {
